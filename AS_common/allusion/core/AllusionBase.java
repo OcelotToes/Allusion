9425302d298f5544c9ace9c22de156bd5ef97c10
Geert4321 <geert.aj.custers@gmail.com>
2013-09-06 20:16:47 +0200

    Forgoot one file... Oops

--- AS_common/allusion/core/AllusionBase.java
+++ AS_common/allusion/core/AllusionBase.java
@@ -1,16 +1,18 @@
 package allusion.core;
 
 import allsion.lib.Reference;
+import allusion.core.handler.ConnectionHandler;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.Init;
 import cpw.mods.fml.common.Mod.PostInit;
+import cpw.mods.fml.common.network.NetworkRegistry;
 
 @Mod(modid = Reference.modid, name = Reference.modName, version = Reference.version)
 
 public class AllusionBase {
 	@Init
 	public void init(){
-		
+		NetworkRegistry.instance().registerConnectionHandler(new ConnectionHandler());
 	}
 	@PostInit
 	public void postInit(){
